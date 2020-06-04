ec036d3644247af14594439cd8330ba6e6989366
Nicolas Waguet <holdgling@gmail.com>
2020-06-04 00:11:23 -0400

    Added jest

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import App, { AppProps } from "../components/App";
+
+describe("<App />", () => {
+  test("should be tested when there will be things to test", async () => {
+  });
+});
